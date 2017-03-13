8abbdfd8225c6d36f7969ba3d25d34fc31545213
Chen Chen <chen.chen@autodesk.com>
2017-03-13 21:35:20 +0800

    Initial Commit

---
+++ src/server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan'); // logger
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('port', (process.env.PORT || 5050));
+
+app.use('/', express.static(__dirname + '/../../src'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(morgan('dev'));
+
+
+// all other routes are handled by Angular
+app.get('/*', function(req, res) {
+    var indexPath = path.join(__dirname,'/../../src/index.html');
+    res.sendFile(indexPath);
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Angular 2 Full Stack listening on port '+app.get('port'));
+});
+
+module.exports = app;
