a2f9c7c7ae5e9e2f95e80b53528a8e5d28d6555f
ADS\chench <chen.chen@autodesk.com>
2017-03-17 18:31:33 +0800

    wrap OSS bucket/object API

--- src/server/buckets.ts
+++ src/server/buckets.ts
@@ -17,6 +17,27 @@ export function route() : express.Router {
             });
         });
     });
+    router.get('/:bucketKey/objects', function (req: express.Request, res: express.Response, next) {
+        auth.authentication().then(function (token: auth.IToken) {
+            getObjects(token.token, req.params.bucketKey)
+            .then(function(objects: IObject[]){
+                res.json(objects);
+            }, function(error){
+                res.sendStatus(400);
+            });
+        });
+    });
+
+    router.get('/:bucketKey/objects/:objectName', function (req: express.Request, res: express.Response, next) {
+        auth.authentication().then(function (token: auth.IToken) {
+            deleteObject(token.token, req.params.bucketKey, req.params.objectName)
+            .then(function(code : number){
+                res.sendStatus(code);
+            }, function(code : number){
+                res.sendStatus(code);
+            });
+        });
+    });
     return router;
 }
 
@@ -48,8 +69,62 @@ function getBuckets(token:string) : Q.Promise<IBucket[]> {
     return deferred.promise;
 }
 
+function getObjects(token:string, bucketKey:string) : Q.Promise<IObject[]> {
+    let deferred = Q.defer<IObject[]>();
+    let objectUrl = config.getBucketURL() + '/' + bucketKey + '/objects';
+    request.get({
+        url: objectUrl,
+        headers: {'Authorization': 'Bearer '+ token}
+   },function (error, response, body) {
+        if (error) {
+            console.log(error);
+            deferred.reject(error);
+        }
+        try {
+            let resobj = JSON.parse(body);
+            let objectList = resobj.items;
+            deferred.resolve(objectList);
+        } catch (error) {
+            console.log(error);
+            deferred.reject(error);
+        }
+    });
+
+    return deferred.promise;
+}
+
+function deleteObject(token:string, bucketKey:string, objectName:string) : Q.Promise<number>{
+    let deferred = Q.defer<number>();
+    let objectUrl = config.getBucketURL() + '/' + bucketKey + '/objects/' + objectName;
+    request.delete({
+        url: objectUrl,
+        headers: {'Authorization': 'Bearer '+ token}
+   },function (error, response, body) {
+        if (error) {
+            console.log(error);
+            deferred.reject(response.statusCode);
+        }
+        try {
+            deferred.resolve(response.statusCode);
+        } catch (error) {
+            console.log(error);
+            deferred.reject(500);
+        }
+    });
+
+    return deferred.promise;
+}
 export interface IBucket {
-    bucketKey: number;
+    bucketKey: string;
     createDate: string;
     policyKey: string;
+}
+
+export interface IObject {
+    bucketKey: string;
+    objectKey: string;
+    objectId:  string;
+    sha1:      string;
+    size:      number;
+    location:  string;
 }
